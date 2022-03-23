7c85a72211a87014aeff3bffb94a749858ce17ac
Shivam Kumar <97663797+DarkEthics@users.noreply.github.com>
2022-03-23 21:19:29 +0530

    Update index.js

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -6,9 +6,33 @@ function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
 
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
+	const GUESS = req.url.split("=").pop(); // Write logic to parse the word which the user guessed from the URL string
+	let feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	let flag=0;
+	
+	function fun(){
+	  for(let i=0;i<GUESS.length;i++){
+		if(GUESS[i]==SECRET[i]){
+			feedback=feedback + "g"; // char found at correct position
+		}
+		
+		else{
+			for(let j=0;j<SECRET.length;j++){
+				if(GUESS[i]==SECRET[j]){
+					feedback=feedback + "y";  //char is found but misplaced
+					flag=1;
+					break;
+				}
+			}
+			
+			if(flag==0)  // required char not found
+			        feedback=feedback + "b";	
+		}  
+		
+	  }	
+	}
+	
+	fun();
 	res.write(feedback);
 	res.end();
 }
